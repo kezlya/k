e1268d156f7662310bdab316b795e6cff727ed4f
Vitaly Kezlya <vitaly@kezlya.com>
2017-12-28 19:33:36 -0800

    Deep refactoring. Not compiled version

--- cmd/main.go
+++ cmd/main.go
@@ -14,13 +14,8 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"github.com/nfnt/resize"
 )
-
-
-
-
-
-
 
 func main() {
 	screen := k.Screen{}
@@ -38,7 +33,6 @@ func main() {
 	//layer3 := getRandomLayer(300, 300)
 	//screen.Add(layer3.current)
 
-
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/stream.jpg", func(w http.ResponseWriter, r *http.Request) {
@@ -50,123 +44,3 @@ func main() {
 	}
 }
 
-func getRandomLayer(width, height int) *k.Layer {
-	return &k.Layer{false,nil,randomPixels(width, height)}
-}
-
-func randomPixels(x, y int) *image.RGBA {
-	sq := image.Rectangle{image.Point{0, 0}, image.Point{x, y}}
-	var img *image.RGBA
-	img = image.NewRGBA(sq)
-
-	for x := 0; x < 500; x++ {
-		for y := 0; y < 500; y++ {
-			r := uint8(x)
-			g := uint8(y)
-			b := uint8(rand.Intn(255))
-			a := uint8(rand.Intn(255))
-			c := color.RGBA{r, g, b, a}
-			img.Set(x, y, c)
-		}
-	}
-	return img
-}
-
-func layerFromImage(keyword string) *k.Layer {
-	var img image.Image
-
-	doc, err := goquery.NewDocument("https://www.google.com/search?q=" + keyword + "&tbm=isch")
-	if err != nil {
-		fmt.Printf(err.Error())
-		log.Fatal(err)
-	}
-
-	// slow on purpose it influence analog like
-	// try 3 times
-
-	for i := 1; i <= 3; i++ {
-		rand.Seed(time.Now().UnixNano())
-		guess := rand.Intn(10)
-		fmt.Println(guess)
-		doc.Find(".images_table").Each(func(index int, item *goquery.Selection) {
-			item.Find("img").Each(func(index2 int, item2 *goquery.Selection) {
-				if index2 == guess {
-					if src, e := item2.Attr("src"); e == true {
-						img = loadJpegFromUrl(src)
-						if img != nil {
-							i = 1000
-						}
-					}
-				}
-			})
-		})
-	}
-
-	return &k.Layer{false,nil, convertYCbCr_RGBA(img.(*image.YCbCr))}
-}
-
-func convertYCbCr_RGBA(img *image.YCbCr) *image.RGBA {
-	b := img.Bounds()
-	m := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
-	draw.Draw(m, m.Bounds(), img, b.Min, draw.Src)
-	return m
-}
-
-func loadJpegFromUrl(url string) image.Image {
-	fmt.Println("load: ", url)
-	res, err := http.Get(url)
-	if err != nil || res.StatusCode != 200 {
-		// handle errors
-	}
-	defer res.Body.Close()
-	m, _, _ := image.Decode(res.Body)
-	return m
-}
-
-func mirror(n int) int {
-	if n > 127 {
-		return n - 127
-	}
-	return n
-}
-
-func fade(n, m int) int {
-	if n > m {
-		return n - m
-	}
-	return 0
-}
-
-func frameNumber(n int) int {
-	if n > 4 {
-		return 0
-	}
-	return n + 1
-}
-
-// Voided function to run whenewer they finish
-/*
-func scaleDown(layer *k.Layer, rate time.Duration, loop bool) {
-	if loop {
-		layer.original = layer.current
-	}
-	for {
-		time.Sleep(rate * time.Millisecond)
-		size := layer.current.Rect.Size()
-		if size.X > 1 && size.Y > 1 {
-			bb := resize.Thumbnail(uint(size.X-5), uint(size.Y-5), layer.current, resize.Bicubic)
-			layer.current = bb.(*image.RGBA)
-			time.Sleep(rate * time.Millisecond)
-		} else {
-			break
-		}
-	}
-	if loop {
-		layer.current = layer.original
-		scaleDown(layer, rate, loop)
-	}
-}
-*/
-func scaleUp(speed, times int) {
-
-}
--- layer.go
+++ layer.go
@@ -1,9 +1,138 @@
 package k
 
-import "image"
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"image"
+	"image/color"
+	"log"
+	"math/rand"
+	"time"
+	"net/http"
+	"github.com/nfnt/resize"
+)
 
 type Layer struct {
-	isLive   bool
-	original *image.RGBA
-	current  *image.RGBA
+	// isLive   bool maybe will use it in the future
+	backup *image.RGBA
+	Still  *image.RGBA
+}
+
+func getRandomLayer(width, height int) *Layer {
+	return &Layer{false, nil, randomPixels(width, height)}
+}
+
+func randomPixels(x, y int) *image.RGBA {
+	sq := image.Rectangle{image.Point{0, 0}, image.Point{x, y}}
+	var img *image.RGBA
+	img = image.NewRGBA(sq)
+
+	for x := 0; x < 500; x++ {
+		for y := 0; y < 500; y++ {
+			r := uint8(x)
+			g := uint8(y)
+			b := uint8(rand.Intn(255))
+			a := uint8(rand.Intn(255))
+			c := color.RGBA{r, g, b, a}
+			img.Set(x, y, c)
+		}
+	}
+	return img
+}
+
+func layerFromImage(keyword string) *k.Layer {
+	var img image.Image
+
+	doc, err := goquery.NewDocument("https://www.google.com/search?q=" + keyword + "&tbm=isch")
+	if err != nil {
+		fmt.Printf(err.Error())
+		log.Fatal(err)
+	}
+
+	// slow on purpose it influence analog like
+	// try 3 times
+
+	for i := 1; i <= 3; i++ {
+		rand.Seed(time.Now().UnixNano())
+		guess := rand.Intn(10)
+		fmt.Println(guess)
+		doc.Find(".images_table").Each(func(index int, item *goquery.Selection) {
+			item.Find("img").Each(func(index2 int, item2 *goquery.Selection) {
+				if index2 == guess {
+					if src, e := item2.Attr("src"); e == true {
+						img = loadJpegFromUrl(src)
+						if img != nil {
+							i = 1000 //TODO: refactor
+						}
+					}
+				}
+			})
+		})
+	}
+
+	return &k.Layer{false, nil, convertYCbCr_RGBA(img.(*image.YCbCr))}
+}
+
+func convertYCbCr_RGBA(img *image.YCbCr) *image.RGBA {
+	b := img.Bounds()
+	m := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+	draw.Draw(m, m.Bounds(), img, b.Min, draw.Src)
+	return m
+}
+
+func loadJpegFromUrl(url string) image.Image {
+	fmt.Println("load: ", url)
+	res, err := http.Get(url)
+	if err != nil || res.StatusCode != 200 {
+		// handle errors
+	}
+	defer res.Body.Close()
+	m, _, _ := image.Decode(res.Body)
+	return m
+}
+
+
+// Voided function to run whenewer they finish
+
+func scaleDown(layer *k.Layer, rate time.Duration, loop bool) {
+	if loop {
+		layer.original = layer.current
+	}
+	for {
+		time.Sleep(rate * time.Millisecond)
+		size := layer.current.Rect.Size()
+		if size.X > 1 && size.Y > 1 {
+			bb := resize.Thumbnail(uint(size.X-5), uint(size.Y-5), layer.current, resize.Bicubic)
+			layer.current = bb.(*image.RGBA)
+			time.Sleep(rate * time.Millisecond)
+		} else {
+			break
+		}
+	}
+	if loop {
+		layer.current = layer.original
+		scaleDown(layer, rate, loop)
+	}
+}
+
+
+
+func scaleUp(speed, times int) {
+
+}
+
+
+
+func mirror(n int) int {
+	if n > 127 {
+		return n - 127
+	}
+	return n
+}
+
+func fade(n, m int) int {
+	if n > m {
+		return n - m
+	}
+	return 0
 }

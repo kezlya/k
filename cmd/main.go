2b774cbcc2f2889164c785eacf87500fc55c713a
Vitaly Kezlya <vitaly@kezlya.com>
2017-12-31 17:26:55 -0800

    It's working yaaaa

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,7 @@ import (
 	"github.com/kezlya/k"
 	"image/jpeg"
 	"log"
-	"math/rand"
 	"net/http"
-	"time"
-	"strconv"
 )
 
 func main() {
@@ -19,11 +16,11 @@ func main() {
 
 	//fmt.Println(layer1)
 
-	rand.Seed(time.Now().UnixNano())
-	guess := rand.Intn(100)
-	layer2 := k.GoogleImage("Number+" + strconv.Itoa(guess),-1)
+	//rand.Seed(time.Now().UnixNano())
+	//guess := rand.Intn(100)
+	//layer2 := k.GoogleImage("Number+" + strconv.Itoa(guess),-1)
 	//go layer2.ScaleDown( 77, true)
-	screen.Add(layer2)
+	//screen.Add(layer2)
 
 	layer3 := k.OnlineImage("http://2fatnerds.com/wp-content/uploads/2013/10/giant-manta-ray.png")
 	screen.Add(layer3)
@@ -31,7 +28,7 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/stream.jpg", func(w http.ResponseWriter, r *http.Request) {
-		jpeg.Encode(w, screen.Display(), &jpeg.Options{80})
+		jpeg.Encode(w, screen.Display(500,500), &jpeg.Options{80})
 	})
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {

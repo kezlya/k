d587fabdae3d6d7e35644612bc0d7b592b451127
Vitaly Kezlya <vitaly@kezlya.com>
2017-12-31 17:35:07 -0800

    Screen have dementions now and cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -4,26 +4,31 @@ import (
 	"github.com/kezlya/k"
 	"image/jpeg"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
+	"strconv"
 )
 
 func main() {
 	screen := k.Screen{}
 
-	//layer1 := k.RandomPixels(100, 100)
-	//go layer1.ScaleDown(150, true)
-	//screen.Add(layer1)
+	layer1 := k.RandomPixels(100, 100)
+	go layer1.ScaleDown(150, true)
+	screen.Add(layer1)
 
-	//fmt.Println(layer1)
-
-	//rand.Seed(time.Now().UnixNano())
-	//guess := rand.Intn(100)
-	//layer2 := k.GoogleImage("Number+" + strconv.Itoa(guess),-1)
-	//go layer2.ScaleDown( 77, true)
-	//screen.Add(layer2)
 
 	layer3 := k.OnlineImage("http://2fatnerds.com/wp-content/uploads/2013/10/giant-manta-ray.png")
+	go layer3.ScaleDown( 33, true)
 	screen.Add(layer3)
+
+	rand.Seed(time.Now().UnixNano())
+	guess := rand.Intn(100)
+	layer2 := k.GoogleImage("Number+" + strconv.Itoa(guess),-1)
+	go layer2.ScaleDown( 77, true)
+	screen.Add(layer2)
+
+
 
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
